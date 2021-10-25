4ab0a872fba8f368da9e4dfbe05e91e11e04e91b
Jurik Ristjouw <msn@jurik.nl>
2021-10-26 01:35:43 +0200

    user story 2 with todos

--- YakHerdAPI/Controllers/YakHerdController.cs
+++ YakHerdAPI/Controllers/YakHerdController.cs
@@ -4,26 +4,75 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using YakHerd;
 
 namespace YakHerdAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("yak-shop/")]
     [ApiController]
     public class YakHerdController : ControllerBase
     {
+        // TODO: figure out entrypoint to / instead of ?
 
         private readonly Herd yakHerd;
 
         public YakHerdController()
         {
-            yakHerd = Herd.ReadHerd(@"Z:\Sollicitatie TalentIncubator\ConsoleYak\herd.xml");
+            yakHerd = YakHerd.Herd.ReadHerd(@"Z:\Sollicitatie TalentIncubator\ConsoleYak\herd.xml");
         }
 
-        [HttpGet]
-        public IEnumerable<LabYak> GetYaks()
+
+        public class HerdFormat
         {
-            return yakHerd.LabYaks;
+            public string Name { get; set; }
+            public decimal Age { get; set; }
+            // TODO: change parametername in output
+            public decimal AgeLastShaved { get; set; }
         }
+
+        [HttpGet("herd/")]
+        public List<HerdFormat> Herd(int daysPast)
+        {
+            yakHerd.CalculateHerd(daysPast);
+
+            var ret = new List<HerdFormat>();
+
+            foreach (LabYak l in yakHerd.LabYaks)
+            {
+                var format = new HerdFormat
+                {
+                    Age = l.Age,
+                    AgeLastShaved = l.LastAgeShaved,
+                    Name = l.Name
+                };
+
+                ret.Add(format);
+            }
+            
+            return ret;
+        }
+
+
+        public class StockFormat
+        {
+            public decimal Milk { get; set; }
+            public int Skins { get; set; }
+        }
+
+        [HttpGet("stock/")]
+        public StockFormat Stock(int T)
+        {
+            yakHerd.CalculateHerd(T);
+
+
+            return new StockFormat
+            {
+                Milk = yakHerd.Milk,
+                Skins = yakHerd.Hides
+            }; 
+        }
+
+
     }
 }
